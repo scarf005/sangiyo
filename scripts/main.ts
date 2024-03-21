6278cee78b1dd6319dddafc03bc768f1af5a52cd
scarf <greenscarf005@gmail.com>
2024-03-21 15:42:40 +0900

    build: update script

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,19 +1,23 @@
 import { fetchJobs } from "./fetch_jobs.ts"
 import { parseJobs } from "./parse_jobs.ts"
 
+const getJobs = (rows: number) => {
+	const endpoint = Deno.env.get("API_ENDPOINT")
+	if (!endpoint) throw new Error("API_ENDPOINT is not set")
+
+	const key = Deno.env.get("API_KEY")
+	if (!key) throw new Error("API_KEY is not set")
+
+	return fetchJobs({ endpoint, key, rows })
+}
+
 if (import.meta.main) {
-	const xmlPath = "./response.xml"
+	const xmlPath = import.meta.dirname + "/response.xml"
 
 	switch (Deno.args[0]) {
 		case "fetch":
 			{
-				const endpoint = Deno.env.get("API_ENDPOINT")
-				if (!endpoint) throw new Error("API_ENDPOINT is not set")
-
-				const key = Deno.env.get("API_KEY")
-				if (!key) throw new Error("API_KEY is not set")
-
-				const res = await fetchJobs({ endpoint, key, rows: 3 })
+				const res = await getJobs(1000)
 
 				await Deno.writeFile(xmlPath, res.body!)
 			}
@@ -27,7 +31,17 @@ if (import.meta.main) {
 				await Deno.writeTextFile("./jobs.json", JSON.stringify(jobs, null, 2))
 			}
 			break
+		case "all":
+			{
+				const res = await getJobs(1000)
+				const jobs = parseJobs(await res.text())
+
+				console.log(jobs)
+
+				await Deno.writeTextFile("./jobs.json", JSON.stringify(jobs, null, 2))
+			}
+			break
 		default:
-			console.log("Invalid command. Use 'fetch' or 'xml'")
+			console.log("Invalid command. Use 'fetch', 'xml' or 'all'")
 	}
 }
