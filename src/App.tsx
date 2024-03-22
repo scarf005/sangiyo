d7c232dc76acb7eee573d3a85080b3b62b403235
scarf <greenscarf005@gmail.com>
2024-03-22 21:46:33 +0900

    feat: job detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useMemo } from "react"
 
-import type { Item } from "../scripts/type.d.ts"
+import type { JobEntry } from "../scripts/type.d.ts"
 import "./App.css"
-
-import json from "../scripts/jobs.json"
-import desc from "../scripts/descriptions.json"
 
 import {
 	Anchor,
@@ -15,25 +12,27 @@ import {
 	MultiSelect,
 	RangeSlider,
 	ScrollArea,
-	SimpleGrid,
 	Stack,
 	Text,
 	Title,
 } from "@mantine/core"
 import { useLocalStorage } from "@mantine/hooks"
+import rawData from "./data.json"
 
-const defaultData: Item[] = json
+const data: Record<string, JobEntry> = rawData
+
+const dataList: JobEntry[] = Object.values(data)
 
 const distinct = <T,>(xs: T[]) => Array.from(new Set(xs))
 
-const 역종분류명 = distinct(defaultData.map((item) => item.역종분류명))
-const 요원구분명 = distinct(defaultData.map((item) => item.요원구분명))
-const 업종구분명 = distinct(defaultData.map((item) => item.업종구분명))
-const 근무지시도 = distinct(defaultData.map((item) => item.근무지시도))
-const 근무지주소 = distinct(defaultData.map((item) => item.근무지주소))
+const 역종분류명 = distinct(dataList.map((item) => item.역종분류명))
+const 요원구분명 = distinct(dataList.map((item) => item.요원구분명))
+const 업종구분명 = distinct(dataList.map((item) => item.업종구분명))
+const 근무지시도 = distinct(dataList.map((item) => item.근무지시도))
+const 근무지주소 = distinct(dataList.map((item) => item.근무지주소))
 
 const pays = distinct(
-	defaultData.flatMap((item) => [item.최소급여, item.최대급여])
+	dataList.flatMap((item) => [item.최소급여, item.최대급여])
 		.filter((x) => x !== 0),
 )
 	.sort((a, b) => a - b)
@@ -41,18 +40,57 @@ const pays = distinct(
 const payMarks = pays
 	.map((x, i) => ({ value: i, label: x }))
 
-const filter = (xs: string | string[], key: keyof Item, data: Item[]): Item[] =>
+const filter = (
+	xs: string | string[],
+	key: keyof JobEntry,
+	data: JobEntry[],
+): JobEntry[] =>
 	xs.length > 0 ? data.filter((item) => xs.includes(item[key] as string)) : data
 
 const useFilter = (
-	data: Item[],
-	filters: { key: keyof Item; xs: string[] }[],
+	data: JobEntry[],
+	filters: { key: keyof JobEntry; xs: string[] }[],
 ) =>
 	useMemo(
 		() => filters.reduce((acc, { key, xs: xs }) => filter(xs, key, acc), data),
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		[data, ...filters.map((filter) => filter.xs)],
 	)
+
+export const JobDetail = ({ id }: { id: number }) => {
+	const x = data[id]
+	return (
+		<article>
+			<Anchor
+				href={`https://work.mma.go.kr/caisBYIS/search/cygonggogeomsaekView.do?cygonggo_no=${x.채용번호}`}
+			>
+				<Title>{x.채용제목}</Title>
+			</Anchor>
+			<Group>
+				<Title order={3}>주소</Title>
+				<Text>{x.주소}</Text>
+			</Group>
+			<Group>
+				<Title order={3}>담당업무</Title>
+				<Text>{x.담당업무}</Text>
+			</Group>
+			<Group>
+				<Title order={3}>출퇴근시간</Title>
+				<Text>{x.출퇴근시간 ?? "정보 없음"}</Text>
+				<Title order={3}>교대근무</Title>
+				<Text>{x.교대근무 ?? "정보 없음"}</Text>
+				<Title order={3}>특근/잔업</Title>
+				<Text>{x.특근_잔업 ?? "정보 없음"}</Text>
+				<Title order={3}>수습기간</Title>
+				<Text>{x.수습기간 ?? "정보 없음"}</Text>
+			</Group>
+			<section>
+				<Title order={3}>비고</Title>
+				<pre>{x.비고}</pre>
+			</section>
+		</article>
+	)
+}
 
 export const App = () => {
 	const [역종, set역종] = useLocalStorage<string[]>({
@@ -79,9 +117,13 @@ export const App = () => {
 		key: "급여검색여부",
 		defaultValue: false,
 	})
+	const [id, setId] = useLocalStorage<number | null>({
+		key: "채용번호",
+		defaultValue: null,
+	})
 
 	// const data = []
-	const data = useFilter(defaultData, [
+	const data = useFilter(dataList, [
 		{ key: "역종분류명", xs: 역종 },
 		{ key: "요원구분명", xs: 요원 },
 		{ key: "업종구분명", xs: 업종 },
@@ -148,6 +190,7 @@ export const App = () => {
 			/>
 		</Stack>
 	)
+
 	return (
 		<main>
 			<Grid overflow="hidden" h="100%">
@@ -162,36 +205,26 @@ export const App = () => {
 				>
 					<SelectGroup />
 					<PaySlider />
-					<Text>총 {defaultData.length}중 {data.length}개</Text>
-					<div style={{ overflowY: "scroll" }}>
-						<Stack>
+					<Text>총 {dataList.length}중 {data.length}개</Text>
+					<ScrollArea>
+						<Stack gap="xs">
 							{data.map((x) => (
-								<Card shadow="xl" key={x.채용번호}>
-									<Anchor
-										href={`https://work.mma.go.kr/caisBYIS/search/cygonggogeomsaekView.do?cygonggo_no=${x.채용번호}`}
-										target="_blank"
-										rel="noreferrer"
-									>
-										<Title order={3}>{x.채용제목}</Title>
-									</Anchor>
-									<Text>
-										{x.업체명} - ({x.근무지주소})
-									</Text>
-									<Anchor
-										href={x.홈페이지주소}
-										target="_blank"
-										rel="noreferrer"
-									>
-										{x.홈페이지주소}
-									</Anchor>
-									<Checkbox />
+								<Card
+									shadow="xl"
+									key={x.채용번호}
+									component="button"
+									onClick={() => setId(x.채용번호)}
+									style={{ display: "flex", alignItems: "flex-start" }}
+								>
+									<Title order={3}>{x.채용제목}</Title>
+									<Text>{x.업체명} - {x.근무지주소}</Text>
 								</Card>
 							))}
 						</Stack>
-					</div>
+					</ScrollArea>
 				</Grid.Col>
 				<Grid.Col span={6}>
-					text
+					{id && <JobDetail id={id} />}
 				</Grid.Col>
 			</Grid>
 		</main>
